5f41f2f907c8ed066ee78a7c93dbdee5ee3ad7e9
yylyingy <yylyingy@sina.com>
2019-04-13 22:07:44 +0800

    支持android上调试xlog

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "916cf60"
-#define MARS_PATH "rb/2019-JAN"
+#define MARS_REVISION "5113d23"
+#define MARS_PATH "HEAD"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-01-23 15:34:54"
+#define MARS_BUILD_TIME "2019-04-09 14:49:06"
 #define MARS_TAG ""
 
 #endif
